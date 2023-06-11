7487ddefa4b918a9144dc56180268088dbbf99ca
mohammedaladhary <caladhary@gmail.com>
2023-06-11 10:34:21 +0400

    arrays

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] num = { 1, 2, 3, 4, 5 };
+        int sum = 0;
+        for (int i = 0; i < num.Length; i++)
+        {
+            sum += num[i];
+        }
+        Console.WriteLine(sum);
+    }
+}

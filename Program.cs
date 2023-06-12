94f6530f25584a7eac8503c7e2121b2ce34c4759
mohammedaladhary <caladhary@gmail.com>
2023-06-12 13:10:14 +0400

    2d arrays

--- Program.cs
+++ Program.cs
@@ -1,9 +1,35 @@
 ﻿using System;
 using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
 using System.Runtime.Intrinsics.X86;
 
 internal class Program
 {
+    /// <summary>
+    /// calculates sum of the array
+    /// </summary>
+    /// <param name="arr">input array </param>
+    /// <returns>sum of the elements of the array</returns>
+    static int sumMultiDiemensional(int[,] arr)
+    {
+        //Write a program that takes a 2D array of integers as input and
+        //calculates the sum of all the elements in the array.
+        //Display the sum as the output.
+
+        int sum = 0;
+
+        for (int row = 0; row < arr.GetLength(0); row++)
+        {
+            for (int column = 0; column < arr.GetLength(1); column++)
+            {
+                //Console.Write(arr[row, column] + " ");
+
+                sum += arr[row, column];
+            }
+        }
+        return sum;
+    }
 
     static int sumArray(int[] num)
     {
@@ -11,12 +37,12 @@ internal class Program
         for (int i = 0; i < num.Length; i++)
         {
             sum += num[i];
-   
+
         }
         return sum;
     }
 
-    static int avgArray(int[] num )
+    static int avgArray(int[] num)
     {
         int sum = 0;
         int avg = 0;
@@ -33,7 +59,7 @@ internal class Program
         int min = 0;
         for (int i = 0; i < num.Length; i++)
         {
-          
+
             if (num[i] < min)
                 min = num[i];
         }
@@ -43,7 +69,7 @@ internal class Program
     public static int maxArray(int[] num)
     {
         int max = 0;
-        for(int i = 0;i < num.Length; i++)
+        for (int i = 0; i < num.Length; i++)
         {
             if (num[i] > max)
             {
@@ -57,7 +83,7 @@ internal class Program
     {
         int countEven = 0;
         int countOdd = 0;
-      
+
         for (int i = 0; i < num.Length; i++)
         {
             if ((int.Parse(num[i].ToString()) % 2) == 0)
@@ -113,34 +139,72 @@ internal class Program
     static void Main(string[] args)
     {
 
+        //Console.WriteLine("enter the array size:");
+        //int n = int.Parse(Console.ReadLine());
+        //int[] arr = new int[n];
 
-        Console.WriteLine("enter the array size:");
-        int n = int.Parse(Console.ReadLine());
-        int[] arr = new int[n];
+        //Console.WriteLine("enter the elements:");
+        //for (int row = 0; row < arr.Length ; row++)
+        //{
+        //    arr[row] = int.Parse(Console.ReadLine());
+        //}
+
+        //Console.WriteLine("the sum is:" + sumArray(arr)); //15
+        //Console.WriteLine("the avg is:" + avgArray(arr)); //3
+
+        //Console.WriteLine("the max is :" + maxArray(arr)); //5
+        //Console.WriteLine("the min is :" + minArray(arr)); //1
+
+        //Console.WriteLine("count for even :" + countArrayEven(arr));
+        //Console.WriteLine("count for odd :" + countArrayOdd(arr));
+
+        ////Console.WriteLine("second largest is :" + secondLargest(arr)); //4
+        //Console.WriteLine("remove duplicate :" + removeDuplicate(arr)); //0
+        //------------------------------------------------------------------
+        //multidimensional
+
+        //int totalRows = 2;
+        //int totalCols = 3;
+        //int[,] numbers = new int[totalRows, totalCols];
+        //numbers[0, 0] = 30;
+        //numbers[0, 1] = 35;
+        //numbers[0, 2] = 40;
+        //numbers[1, 0] = 45;
+        //numbers[1, 1] = 50;
+        //numbers[1, 2] = 55;
 
 
-        Console.WriteLine("enter the elements:");
-        for (int i = 0; i < arr.Length ; i++)
-        {
-            arr[i] = int.Parse(Console.ReadLine()) ;
-        }
 
-        Console.WriteLine("the sum is:" + sumArray(arr)); //15
-        Console.WriteLine("the avg is:" + avgArray(arr)); //3
+        //for (int row = 0; row < numbers.GetLength(0); row++)
+        //{
+        //    Console.Write("Row " + row + ": ");
 
-        Console.WriteLine("the max is :" + maxArray(arr)); //5
-        Console.WriteLine("the min is :" + minArray(arr)); //1
+        //    for (int column = 0; column < numbers.GetLength(1); column++)
+        //    {
+        //        Console.Write(numbers[row, column] + " ");
 
-        Console.WriteLine("count for even :" + countArrayEven(arr));
-        Console.WriteLine("count for odd :" + countArrayOdd(arr));
+        //    }
+        //    Console.WriteLine();
 
-        //Console.WriteLine("second largest is :" + secondLargest(arr)); //4
-        Console.WriteLine("remove duplicate :" + removeDuplicate(arr)); //0
+        //}
+        //--------------------------------------------------------------
 
+        //--------------------------------------------------
+        //Write a program that takes a 2D array of integers as input and
+        //calculates the average value for each row.
+        //Display the average value of each row as the output.
 
+            int totalRows = 3;
+            int totalCols = 2;
+            int[,] tra = new int[totalRows, totalCols];
+            tra[0, 0] = 30;
+            tra[0, 1] = 35;
+            tra[1, 0] = 40;
+            tra[1, 1] = 45;
+            tra[2, 0] = 50;
+            tra[2, 1] = 55;
+            Console.WriteLine("The sum is :"+sumMultiDiemensional(tra));
 
 
     }
-
-
 }
